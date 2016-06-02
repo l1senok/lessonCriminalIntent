d3d4c2ca4b18bc512f2f32a51f475942f6ded9dc
Alesya Volosach <alesya.volosach@gmail.com>
2016-06-03 00:47:29 +0300

    Chapter 9.complete (list filling)

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -7,7 +7,9 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -15,7 +17,7 @@ import java.util.List;
  * Created by alice on 02.06.16.
  */
 public class CrimeListFragment extends Fragment{
-    //ViewHolder прописан здесь
+    //ViewHolder и Adapter прописан здесь
 
     private RecyclerView mCrimeRecyclerView;
     private CrimeAdapter mAdapter;
@@ -41,15 +43,36 @@ public class CrimeListFragment extends Fragment{
     }
 
     //ViewHolder
-    private class CrimeHolder extends RecyclerView.ViewHolder{
+    private class CrimeHolder extends RecyclerView.ViewHolder
+    implements View.OnClickListener{
         //Хранит ссылку на одно представление, ожидается что itemView относится к типу TextView
 
+        private Crime mCrime;
+
         //Поля вьюшек которыми заполняется RV
-        public TextView mTitleTextView;
+        private TextView mTitleTextView;
+        private TextView mDateTextView;
+        private CheckBox mSolvedCheckBox;
 
         public CrimeHolder(View itemView) {
             super(itemView);
-            mTitleTextView = (TextView) itemView;
+            itemView.setOnClickListener(this);
+
+            mTitleTextView = (TextView) itemView.findViewById(R.id.list_item_crime_title_text_view);
+            mDateTextView = (TextView) itemView.findViewById(R.id.list_item_crime_data_title_text_view);
+            mSolvedCheckBox = (CheckBox) itemView.findViewById(R.id.list_item_crime_solved_check_box);
+        }
+        public void bindCrime(Crime crime) {
+            mCrime = crime;
+            mTitleTextView.setText(mCrime.getTitle());
+            mDateTextView.setText(mCrime.getDate().toString());
+            mSolvedCheckBox.setChecked(mCrime.isSolved());
+        }
+
+        @Override
+        public void onClick(View v) {
+            //Высвечивание сообщения в случай нажатия на элемент списка
+            Toast.makeText(getActivity(),mCrime.getTitle() + " clicled!", Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -66,7 +89,7 @@ public class CrimeListFragment extends Fragment{
         public CrimeHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             //вызывается,когда требуется новое представление для отображения элемента
             LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
-            View view = layoutInflater.inflate(android.R.layout.simple_list_item_1,parent,false);
+            View view = layoutInflater.inflate(R.layout.list_item_crime,parent,false);
 
             return new CrimeHolder(view);
         }
@@ -75,7 +98,7 @@ public class CrimeListFragment extends Fragment{
         public void onBindViewHolder(CrimeHolder holder, int position) {
             //связываем представление view обекта VH с объектом модели
             Crime crime = mCrimes.get(position);
-            holder.mTitleTextView.setText(crime.getTitle());
+            holder.bindCrime(crime);
         }
 
         @Override
